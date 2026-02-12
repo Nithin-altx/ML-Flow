e9f45ae7e8ccc7034306ccd7af5654b8f35bdd51
Nithin <nithin200133@gmail.com>
2026-02-12 17:59:12 +0530

    Complete ml-flow

---
+++ src/file1.py
@@ -0,0 +1,37 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+# import seaborn as sns
+
+wine=load_wine()
+X = wine.data
+y = wine.target
+
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.10,random_state=42)
+
+max_depth=10
+n_estimators=7
+
+mlflow.set_experiment('Randomforest')
+
+with mlflow.start_run():
+    rf=RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth,random_state=42)
+    rf.fit(X_train,y_train)
+    y_pred=rf.predict(X_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+    mlflow.log_artifact(__file__)
+    mlflow.set_tags({'Author':'Nithin','Project':'random forest'})
+    mlflow.sklearn.log_model(rf, "Random-Forest-Model")
+
+
+    print(accuracy)
+
+
